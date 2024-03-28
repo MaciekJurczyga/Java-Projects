f9ebe39b3e010e6ef8c64bac462385ac00ef7b38
Maciej <maciekjur123@gmail.com>
2024-03-28 01:23:18 +0100

    basic implementation of LinkedList

---
+++ LinkedListImpl/src/LinkedList.java
@@ -0,0 +1,51 @@
+public class LinkedList {
+    Node head;
+
+    private static class Node{
+        int val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+            next = null;
+        }
+    }
+    public void insert(int val){
+        if(head == null){
+            head = new Node(val);
+        }
+        else{
+            Node cur = head;
+            while(cur.next != null){
+                cur = cur.next;
+            }
+            cur.next = new Node(val);
+        }
+    }
+    public void remove(int val){
+        Node cur = head;
+        Node prev = null;
+        while(cur!=null){
+            if(cur.val == val){
+                if(prev == null){
+                    head = cur.next;
+                }
+                else{
+                    prev.next = cur.next;
+                }
+                return;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+    }
+
+    public void display(){
+        Node cur = head;
+        while(cur != null){
+            System.out.print(cur.val + " -> ");
+            cur = cur.next;
+        }
+        System.out.print(" null");
+    }
+}
---
+++ LinkedListImpl/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insert(5);
+        linkedList.insert(2);
+        linkedList.insert(3);
+        linkedList.insert(11);
+        linkedList.insert(6);
+
+        linkedList.display();
+        System.out.println();
+        linkedList.remove(6);
+
+        linkedList.display();
+    }
+}
